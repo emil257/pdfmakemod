8fe3813d8599f420a46cdde4d7d14a8d7ecc5433
emil257 <emil.saxlund@live.se>
2022-03-09 16:12:21 +0100

    emil

--- src/textTools.js
+++ src/textTools.js
@@ -326,7 +326,7 @@ function measure(fontProvider, textArray, styleContextStack) {
 		var font = fontProvider.provideFont(fontName, bold, italics);
 
 		item.width = widthOfString(item.text, font, fontSize, characterSpacing, fontFeatures);
-		item.height = fontSize * lineHeight;
+		item.height = (fontSize < 14 ? 14 : fontSize) * lineHeight;
 
 		if (!item.leadingCut) {
 			item.leadingCut = 0;
